9609e7d19f8e864ea111e276b588fa05cf5366a0
Yohsuke Yamakawa <yyamakawa@gmail.com>
2015-12-02 16:39:27 -0500

    update

--- weatherNode/weatherNode.js
+++ weatherNode/weatherNode.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var apikey = 'e312dbeb8840e51f92334498a261ca1d';
 var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial&APPID="+apikey;
+
 var body = "";
 var request = http.get(weatherUrl, function(response){
 	console.log(response.statusCode);
